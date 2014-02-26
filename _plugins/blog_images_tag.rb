28d1fcb4cc3c0f2d5da2278dff0ec6998b1fb82f
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-02-26 19:16:33 +1300

    Adds image description and refactoring

--- _plugins/blog_images_tag.rb
+++ _plugins/blog_images_tag.rb
@@ -1,25 +1,49 @@
 module Jekyll
-  class BlogImagesTag < Liquid::Tag
+  module Converters
+    class BlogImagesTag < Liquid::Tag
 
-    def render(context)
-      result_tag = ""
-      img_folder = context["page"]["id"].gsub('/', '-')
-      img_folder[0] = ''
-      img_folder = "img/#{img_folder}/thumb/*.jpg"
-      imgs = Dir[img_folder].sort_by { |x| x.gsub(/[^\d]/, '').to_i }
-      imgs.each do |img|
+      def img_number img
+        img.match(/.*\/(\d+).*\.jpg$/)[1].to_i
+      end
+
+      def thumb_dir context
+        id_path = context["page"]["id"].gsub('/', '-')
+        id_path[0] = ''
+        thumb_dir = "img/#{id_path}/thumb"
+      end
+
+      def img_tag img
         case img
         when /-left.jpg$/
-          result_tag << "<div class='col-lg-6 col-md-6 col-xs-6 img-col-left'><img class='img-responsive' src='/#{img}' /></div>"
+          "<div class='col-lg-6 col-md-6 col-xs-6 img-col-left'><img class='img-responsive' src='/#{img}' /></div>"
         when /-right.jpg$/
-          result_tag << "<div class='col-lg-6 col-md-6 col-xs-6 img-col-right'><img class='img-responsive' src='/#{img}' /></div>"
+          "<div class='col-lg-6 col-md-6 col-xs-6 img-col-right'><img class='img-responsive' src='/#{img}' /></div>"
         else
-          result_tag << "<img class='img-responsive' src='/#{img}' />"
+          "<img class='img-responsive' src='/#{img}' />"
         end
       end
-      result_tag
+
+      def img_description_tag img, context
+        description_file = "#{thumb_dir context}/#{img.split('/')[-1].split('.')[0]}.md"
+        if File.exist? description_file
+          markdown_converter = context.registers[:site].converters.find { |c| c.matches(".md") }
+          "<div class='img-description'>#{markdown_converter.convert(IO.read(description_file))}</div>"
+        else
+          ""
+        end
+      end
+
+
+      def render(context)
+        result_tag = ""
+        Dir["#{thumb_dir context}/*.jpg"].sort_by { |img| img_number(img) }.each do |img|
+          result_tag << img_tag(img)+ img_description_tag(img, context)
+        end
+        result_tag
+      end
+
     end
   end
 end
 
-Liquid::Template.register_tag('blog_images', Jekyll::BlogImagesTag)
+Liquid::Template.register_tag('blog_images', Jekyll::Converters::BlogImagesTag)
